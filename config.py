0dda7f315eb06693043de3554ce67a1537f5c515
AnimeSenseiNetwork <131219940+AnimeSenseiNetwork@users.noreply.github.com>
2024-12-03 19:00:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}!⚡\n\n ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ʙᴏᴛʜ ᴏꜰ ᴏᴜʀ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟꜱ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ...!")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}!⚡\n\n ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ @Anime_Sensei_Network ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ...!")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
